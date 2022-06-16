787c876224aa8c321e91750413685f4dd86770a4
Hans Zizold <hzizold@hotmail.com>
2022-06-16 19:40:34 -0400

    Linter corrections

--- modules/__mocks__/checkboxformat.js
+++ modules/__mocks__/checkboxformat.js
@@ -1,10 +1,10 @@
 // Function to give format when pressing some checkbox
 // Also it changes the status of a task (true/false) and update LS
 const checkboxFormat = (element) => {
-    // Mark task completed and update LS
-    const completedindex = element;
-    const lsTasks = JSON.parse(localStorage.getItem('mytests'));
-    lsTasks[completedindex-1].completed = !lsTasks[completedindex-1].completed;
-    localStorage.setItem('mytests', JSON.stringify(lsTasks));
+  // Mark task completed and update LS
+  const completedindex = element;
+  const lsTasks = JSON.parse(localStorage.getItem('mytests'));
+  lsTasks[completedindex - 1].completed = !lsTasks[completedindex - 1].completed;
+  localStorage.setItem('mytests', JSON.stringify(lsTasks));
 };
 export default checkboxFormat;
